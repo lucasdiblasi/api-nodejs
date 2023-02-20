2cb0fafe0bdf1b3ccc111013b9b57d38d68d35e2
Lucas Di Blasi <lucasdiblasi.ti@hotmail.com>
2023-02-20 16:51:31 -0300

    middlewares

--- index.js
+++ index.js
@@ -6,25 +6,53 @@ server.use(express.json())
 
 const bebidas = ['Energético monster', 'Red Bull', 'Taurine'];
 
-server.get('/bebidas', (request, response) => {
+server.use((request, response, next) => {
+    return next();
+})
+
+// Bad Request
+function checkBebidas(request, response, next) {
+    if(!request.body.name){
+        return response.status(400).json({ error: "Nome do produto é obrigatório"})
+    }
+     
+    return next()
+}
+
+function checkBebidasIndex(request, response, next) {
+    const bebidaID = bebidas[request.params.index]
+
+    if(!bebidaID) {
+        return response.status(400).json({ error: "Produto Inexistente"})
+    }
+     
+    return next()
+}
+
+
+//Get all products
+server.get('/bebidas',  (request, response) => {
     return response.json(bebidas)
 })
 
-server.get('/bebidas/:index', (request, response) => {
+// Get just one product
+server.get('/bebidas/:index', checkBebidasIndex, (request, response) => {
 
     const { index } = request.params
     return response.json(bebidas[index])
 
 })
 
-server.post('/bebidas', (request, response) => {
+//Create a new product
+server.post('/bebidas', checkBebidas, (request, response) => {
     const { name } = request.body
     bebidas.push(name)
 
     return response.json({message: `${name} cadastrado com sucesso`})
 })
 
-server.put('/bebidas/:index', (request, response) => {
+// Update a product
+server.put('/bebidas/:index', checkBebidas, (request, response) => {
     const { index } = request.params
     const { name } = request.body
 
@@ -33,7 +61,8 @@ server.put('/bebidas/:index', (request, response) => {
     return response.json(bebidas)
 })
 
-server.delete('/bebidas/:index', (request, response) => {
+//Delete a product
+server.delete('/bebidas/:index', checkBebidasIndex, (request, response) => {
     const { index } = request.params
     
     bebidas.splice(index, 1)
